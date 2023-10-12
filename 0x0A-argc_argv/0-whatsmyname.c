772b62eab5e61a2dd5bc1ec9f02879afb0a6485b
zakisw <youssefakisw@gmail.com>
2023-10-12 12:47:50 -0700

    okii

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
-/*prints the name of the program*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ * @argc : argument count
+ * @argv : argument vector
+ */
 int main(int argc, char *argv[])
 {
 printf("%s\n", argv[0]);
