22b32d8e14a86087684dd95f9ee7636ac3794c91
zakisw <youssefakisw@gmail.com>
2023-10-12 12:55:37 -0700

    lo

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ * @argc : argument count
+ * @argv : argument vector
+ */
+int main(int argc, char __attribute__((unused))*argv[])
+{
+printf("%d\n", argc);
+return (0);
+}
