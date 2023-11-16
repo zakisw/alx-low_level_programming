cef09efc59501c89971d52fde5a84b0d98b0a338
zakisw <youssefakisw@gmail.com>
2023-11-16 08:54:13 -0800

    2.2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -12,13 +12,11 @@ int get_bit(unsigned long int n, unsigned int index)
 	unsigned long int divisor, check;
 
 	if (index > (sizeof(unsigned long int) * 8 - 1))
-	{	return (-1);
+		return (-1);
 	divisor = 1 << index;
 	check = n & divisor;
-	}
-	else if (check == divisor)
-	{	return (1);
-	}
+	if (check == divisor)
+		return (1);
 	return (0);
 }
 
